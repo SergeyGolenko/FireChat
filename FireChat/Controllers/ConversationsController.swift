60899bea04f651cbf610813065b978197f9a3179
SergeyGolenko <1989sergiotay@gmail.com>
2020-12-29 14:00:09 +0200

    tableView,dataSurce,delefateTableView

--- FireChat/Controllers/ConversationsController.swift
+++ FireChat/Controllers/ConversationsController.swift
@@ -13,7 +13,8 @@ class ConversationsController: UIViewController{
     
     
     //MARK: - Properties
-    
+    private let tableView = UITableView()
+     private let reuseIdentifier = "54"
     
     
     //MARK: - Lifecycle
@@ -21,6 +22,7 @@ class ConversationsController: UIViewController{
         super.viewDidLoad()
         configureUI()
         configureNavigationBar()
+        configureTableView()
       
     }
     
@@ -50,12 +52,39 @@ class ConversationsController: UIViewController{
         navigationController?.navigationBar.tintColor = .white
         navigationController?.navigationBar.isTranslucent = true
         navigationController?.navigationBar.overrideUserInterfaceStyle = .dark
-        
-        
-        
-        
+    }
+    
+    func configureTableView(){
+        tableView.backgroundColor = .white
+        tableView.rowHeight = 80
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.tableFooterView = UIView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.frame = view.frame
+        view.addSubview(tableView)
     }
 
+    
+}
+
+
+extension ConversationsController: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("it is - \(indexPath.row)")
+    }
+}
+
+extension ConversationsController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier,for: indexPath)
+        return cell
+    }
+    
     
 }
 
