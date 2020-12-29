41c4708ead490745e6d143bf7d2fa8c29ba0efcf
SergeyGolenko <1989sergiotay@gmail.com>
2020-12-29 12:14:42 +0200

    add new file controller ConversationController

---
+++ FireChat/Controllers/ConversationsController.swift
@@ -0,0 +1,29 @@
+//
+//  ConversationsController.swift
+//  FireChat
+//
+//  Created by Сергей Голенко on 29.12.2020.
+//
+
+import UIKit
+
+class ConversationsController: UIViewController{
+    
+    
+    
+    
+    //MARK: - Properties
+    
+    
+    
+    //MARK: - Lifecycle
+    
+    
+    
+    //MARK: - Helpers
+    
+    
+    
+}
+
+
